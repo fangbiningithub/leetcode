c3834d432585179dfd635e4224202e86f6deaa0f
fangbinbin <fangbinbin@kuaishou.com>
2021-12-06 17:53:04 +0800

    1

---
+++ src/main/java/com/fangin/leetcode/sort/BinaryTree.java
@@ -0,0 +1,11 @@
+package com.fangin.leetcode.sort;
+
+/**
+ * @author fangbinbin <fangbinbin@kuaishou.com>
+ * Created on 2021-12-06
+ */
+public class BinaryTree {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
